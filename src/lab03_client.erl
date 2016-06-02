0f22ff9657f6635ca3e2b98df785fdff526c0317
David Villa <david.villa@heig-vd.ch>
2016-06-02 11:27:45 +0200

    Creation projet

---
+++ src/lab03_client.erl
@@ -0,0 +1,18 @@
+-module(lab03_client).
+
+%% lab03_client: lab03_client library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
